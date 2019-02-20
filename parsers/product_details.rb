a6ea87fdf3ff9972d35e4eb9fe10265ccb1d7216
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-20 10:58:23 +0100

    Update scraper.

--- parsers/product_details.rb
+++ parsers/product_details.rb
@@ -13,7 +13,35 @@ category = data['category']['name']
 
 description = json_details['info']['PRODUCT_DESCRIPTION'].gsub(/[\n\s]+/, ' ').gsub(/,/, '.') rescue ''
 pack = data['amountInPackage'].to_i>0?data['amountInPackage']:"1"
+size =  data['quantity']
+size_uom = data['unit']
+if pack>0
 
+  regexps = [
+      /(\d*[\.,]?\d+)\s?([Ff][Ll]\.?\s?[Oo][Zz])/,
+      /(\d*[\.,]?\d+)\s?([Oo][Zz])/,
+      /(\d*[\.,]?\d+)\s?([Ff][Oo])/,
+      /(\d*[\.,]?\d+)\s?([Ee][Aa])/,
+      /(\d*[\.,]?\d+)\s?([Ff][Zz])/,
+      /(\d*[\.,]?\d+)\s?(Fluid Ounces?)/,
+      /(\d*[\.,]?\d+)\s?([Oo]unce)/,
+      /(\d*[\.,]?\d+)\s?([Mm][Ll])/,
+      /(\d*[\.,]?\d+)\s?([Cc][Ll])/,
+      /(\d*[\.,]?\d+)\s?([Ll])/,
+      /(\d*[\.,]?\d+)\s?([Gg])/,
+      /(\d*[\.,]?\d+)\s?([Ll]itre)/,
+      /(\d*[\.,]?\d+)\s?([Ss]ervings)/,
+      /(\d*[\.,]?\d+)\s?([Pp]acket\(?s?\)?)/,
+      /(\d*[\.,]?\d+)\s?([Cc]apsules)/,
+      /(\d*[\.,]?\d+)\s?([Tt]ablets)/,
+      /(\d*[\.,]?\d+)\s?([Tt]ubes)/,
+      /(\d*[\.,]?\d+)\s?([Cc]hews)/,
+      /(\d*[\.,]?\d+)\s?([Mm]illiliter)/i,
+  ]
+  regexps.find {|regexp| data['fullName'] =~ regexp}
+  size = $1
+  size_uom = $2
+end
 product_details = {
     # - - - - - - - - - - -
     RETAILER_ID: '102',
@@ -31,12 +59,12 @@ product_details = {
     PRODUCT_RANK: page['vars']['product_rank'],
     PRODUCT_PAGE: page['vars']['page'],
     PRODUCT_ID: data['id'],
-    PRODUCT_NAME: data['name'],
+    PRODUCT_NAME: data['fullName'],
     EAN: data['gtin'],
     PRODUCT_DESCRIPTION: description,
     PRODUCT_MAIN_IMAGE_URL: data['images']['ORIGINAL'],
-    PRODUCT_ITEM_SIZE: data['quantity'],
-    PRODUCT_ITEM_SIZE_UOM: data['unit'],
+    PRODUCT_ITEM_SIZE: size,
+    PRODUCT_ITEM_SIZE_UOM: size_uom,
     PRODUCT_ITEM_QTY_IN_PACK: pack,
     SALES_PRICE: price,
     IS_AVAILABLE: availability,
